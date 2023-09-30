35d7c56053b9545f5dc4ab501860c9fdcafa0127
Hardik kachhwaha <hardik.kachhwaha18@gmail.com>
2023-10-01 00:22:46 +1345

    first commit

--- old.js
+++ old.js
@@ -79,19 +79,3 @@
 //      })
 //  })
 //  console.log("starting new")
-// const http = require('http');
-// const server =http.createServer((req,res)=>{
-
-// if(req.url ==='/'){
-//     res.end("welcome ram")
-// }
-// if(req.url ==='/about'){
-//     res.end("krishna about")
-// }
-// res.end(`
-// <h1>oops</h1>
-// <p>no man dont do that </p>
-// <a href="/">back home</a>
-// `)
-// })
-// server.listen(5000)
